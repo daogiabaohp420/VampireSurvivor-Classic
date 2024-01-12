b0385c72d15c5e122e1cfcbe6e142f7ff0d4d49e
daogiabaohp420 <daogiabaohp420@gmail.com>
2024-01-12 11:39:46 +0100

    Add animation for player
    Add walk and idle animation for player.

--- player.gd
+++ player.gd
@@ -1,6 +1,13 @@
 extends CharacterBody2D
 
 func _physics_process(delta):
+	# Input.get_vector : character can move by W,A,S,D
 	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
 	velocity = direction * 500
+	#Character con collide when they meet wall by move_and_slide()
 	move_and_slide()
+	#get_node give us access to node we want
+	if velocity.length() > 0.0:
+		$HappyBoo.play_walk_animation()
+	else :
+		$HappyBoo.play_idle_animation()
