cf72ea4c4790bc05ea21bd74739b6b763376db0c
daogiabaohp420 <daogiabaohp420@gmail.com>
2024-01-12 15:58:24 +0100

    Add a main scene and enviroment on this
    Add a main scene include character , object and a colored backround.

--- player.gd
+++ player.gd
@@ -8,6 +8,7 @@ func _physics_process(delta):
 	move_and_slide()
 	#get_node give us access to node we want
 	if velocity.length() > 0.0:
+		#$ shortcut for get_node() , use % if the node is reparent of another node
 		$HappyBoo.play_walk_animation()
 	else :
 		$HappyBoo.play_idle_animation()
