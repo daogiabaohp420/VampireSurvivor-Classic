082fdd5135c563db80c21cf2bc7540b3b020faa4
daogiabaohp420 <daogiabaohp420@gmail.com>
2024-01-17 14:49:09 +0100

    Fixed the ERROR
    add / before root

--- monster.gd
+++ monster.gd
@@ -1,12 +1,11 @@
 extends CharacterBody2D
 
-var player
 
-func _ready():
-	player = get_node("root/Game/Player")
+@onready var player = get_node("/root/Game/Player")
+
+
 
 func _physics_process(delta):
-	#Create a direction for monster to chase player.
-	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * 350.0 
+	var direction =global_position.direction_to(player.global_position)
+	velocity = direction * 350
 	move_and_slide()
