8ae904b4460f430c07088f368e22628b58282c51
daogiabaohp420 <daogiabaohp420@gmail.com>
2024-01-18 11:35:02 +0100

    Add script to gun and change layer and mask on Gun and Monster and mask on
    In the script. we write a a code that make the gun automatically aim in Monster. In the gun inspector change layer to 0 and mask to 2 . In the monster inspector change layer to 2 and mask to 1 and 2.

--- gun.gd
+++ gun.gd
@@ -1,1 +1,10 @@
 extends Area2D
+
+
+func _physics_process(delta):
+	#find all of characters body 2D nodes that overlapping
+	var enemies_in_range = get_overlapping_bodies()
+	if enemies_in_range.size() > 0:
+		# .front() = [0]
+		var target_enemy = enemies_in_range.front()
+		look_at(target_enemy.global_position)
