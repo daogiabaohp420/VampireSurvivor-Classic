8133ae5afea53a888b2d4b2a6bc192e03ba06c00
daogiabaohp420 <daogiabaohp420@gmail.com>
2024-01-24 14:59:11 +0100

    Create health system for player
    Create var health for player, Add area2d to detect when monster touch player .
    Add some code to rate the damage when mutiple monster touch player and Create one health bar.

--- player.gd
+++ player.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
 
+var health = 100.0
+# Create a custom signal
+signal health_depleted 
+
 func _physics_process(delta):
 	# Input.get_vector : character can move by W,A,S,D
 	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
@@ -12,3 +16,13 @@ func _physics_process(delta):
 		$HappyBoo.play_walk_animation()
 	else :
 		$HappyBoo.play_idle_animation()
+		
+	const DAMAGE_RATE = 5.0
+	# get_overlapping_bodies give a list of array of  overlapping slimes
+	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
+	if overlapping_mobs.size() > 0:
+		health -= DAMAGE_RATE * overlapping_mobs.size() * delta 
+		%HealthBar.value = health
+		
+		if health <= 0.0 :
+			health_depleted.emit()
